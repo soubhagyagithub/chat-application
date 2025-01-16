1acf966c04ca1def2fe1b15257b1f9e95fe34fc3
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2025-01-16 22:47:06 +0530

    Creating Login Screen Frontend

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,6 @@
-const signUp = document.getElementById("signUp");
-const signIn = document.getElementById("signIn");
-const container = document.getElementById("container");
-const signUpBtn = document.getElementById("signUpBtn");
 const loginBtn = document.getElementById("loginBtn");
 const loginEmail = document.getElementById("loginEmail");
 const loginPassword = document.getElementById("loginPassword");
-
-signUp.addEventListener("click", () => {
-  container.classList.add("active");
-});
-
-signIn.addEventListener("click", () => {
-  container.classList.remove("active");
-});
-
-function userSignUp(event) {
-  event.preventDefault();
-  const signUpDetails = {
-    userName: event.target.name.value,
-    userEmail: event.target.email.value,
-    userNumber: event.target.number.value,
-    userPassword: event.target.password.value,
-  };
-  console.log(signUpDetails);
-  axios
-    .post("http://localhost:4000/user/signUp", signUpDetails)
-    .then((res) => {
-      alert(res.data.message);
-      window.location.href = "/";
-    })
-    .catch((error) => {
-      if (error.response) {
-        const errorMessage = error.response.data.message;
-        alert(errorMessage);
-      } else {
-        alert("An error occurred. Please try again later.");
-      }
-    });
-}
 
 function login() {
   const loginDetails = {
